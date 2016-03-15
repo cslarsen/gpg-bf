a594a01f81170fd6cc48f4e7160e66f040b2c5d7
Christian Stigen Larsen <csl@csl.name>
2016-03-16 00:24:02 +0100

    Refactored

--- gpgbf.py
+++ gpgbf.py
@@ -12,7 +12,7 @@ import sys
 def bruteforce(filename, version, data):
     pass
 
-def parse(file):
+def parse_ascii_armored_file(file):
     data = []
     parsing = False
     version = None
@@ -27,109 +27,155 @@ def parse(file):
                 break
             elif len(line.strip()) > 0:
                 data.append(line.strip())
-    return file.name, version, base64.decodestring("".join(data))
+    data = base64.decodestring("".join(data))
+    data = map(ord, data)
+    return version, data
 
-def nibbles(bytes):
-    out = []
-    for byte in bytes:
-        out.append((byte & 0xf0) >> 4)
-        out.append(byte & 0x0f)
-    return out
+class Parser(object):
+    def __init__(self, version, data):
+        self.file_version = version
+        self.contents = {}
 
-def decode(filename, version, data):
-    print("Filename: %s" % filename)
-    print("Version: %s" % version)
-    print("Bytes: %d" % len(data))
+        self.tags = {
+            0: "Reserved - a packet tag MUST NOT have this value",
+            1: "Public-Key Encrypted Session Key Packet",
+            2: "Signature Packet",
+            3: "Symmetric-Key Encrypted Session Key Packet",
+            4: "One-Pass Signature Packet",
+            5: "Secret-Key Packet",
+            6: "Public-Key Packet",
+            7: "Secret-Subkey Packet",
+            8: "Compressed Data Packet",
+            9: "Symmetrically Encrypted Data Packet",
+           10: "Marker Packet",
+           11: "Literal Data Packet",
+           12: "Trust Packet",
+           13: "User ID Packet",
+           14: "Public-Subkey Packet",
+           17: "User Attribute Packet",
+           18: "Sym. Encrypted and Integrity Protected Data Packet",
+           19: "Modification Detection Code Packet",
+           60: "Private or Experimental Values",
+           61: "Private or Experimental Values",
+           62: "Private or Experimental Values",
+           63: "Private or Experimental Values",
+        }
 
-    raw = map(ord, data)
-    ptag = int(raw[0])
-    print("PTag raw: 0x%02x" % ptag)
-    assert (ptag & 0x80) == 0x80, "Incorrect PTag"
-    print("PTag header OK")
-    assert (ptag & 0x40) == 0, "Unsupported packet format"
-    print("Old format OK")
+        self.pk_algos = {
+            1: "RSA (Encrypt or Sign) [HAC]",
+            2: "RSA Encrypt-Only [HAC]",
+            3: "RSA Sign-Only [HAC]",
+           16: "Elgamal (Encrypt-Only) [ELGAMAL] [HAC]",
+           17: "DSA (Digital Signature Algorithm) [FIPS186] [HAC]",
+           18: "Reserved for Elliptic Curve",
+           19: "Reserved for ECDSA",
+           20: "Reserved (formerly Elgamal Encrypt or Sign)",
+           21: "Reserved for Diffie-Hellman (X9.42, as defined for IETF-S/MIME)",
+          100: "Private/Experimental algorithm",
+          101: "Private/Experimental algorithm",
+          102: "Private/Experimental algorithm",
+          103: "Private/Experimental algorithm",
+          104: "Private/Experimental algorithm",
+          105: "Private/Experimental algorithm",
+          106: "Private/Experimental algorithm",
+          107: "Private/Experimental algorithm",
+          108: "Private/Experimental algorithm",
+          109: "Private/Experimental algorithm",
+          110: "Private/Experimental algorithm",
+        }
 
-    tag = (ptag & 0x3c) >> 2
-    print("PTag: 0x%02x" % tag)
+        self._parse(version, data)
 
-    ltype = (ptag & 0x03)
-    print("Length-type raw: 0x%02x" % ltype)
-    plen, hlen = {0: (1,2), 1: (2,3), 3: (-1, 1)}.get(ltype)
-    print("Packet has a %d-octet length" % plen)
-    print("The header is %d-octet%s long" % (hlen, "s" if hlen==0 else ""))
+    def _parse(self, version, data):
+        print("Version: %s" % version)
+        print("Bytes: %d" % len(data))
 
-    assert ltype == 0, "Unsupported packet lengh-type"
-    bodylen = raw[1]
-    print("Body length: %d octets" % bodylen)
+        tag, body = self.parse_packet_header(data)
+        return self.parse_packet_contents(tag, body)
 
-    ptag = (raw[0] & 0x3c) >> 2
-    assert (ptag != 0), "Reserved - a packet tag MUST NOT have this value"
-    print("Packet tag: %s" % {
-         0: "Reserved - a packet tag MUST NOT have this value",
-         1: "Public-Key Encrypted Session Key Packet",
-         2: "Signature Packet",
-         3: "Symmetric-Key Encrypted Session Key Packet",
-         4: "One-Pass Signature Packet",
-         5: "Secret-Key Packet",
-         6: "Public-Key Packet",
-         7: "Secret-Subkey Packet",
-         8: "Compressed Data Packet",
-         9: "Symmetrically Encrypted Data Packet",
-        10: "Marker Packet",
-        11: "Literal Data Packet",
-        12: "Trust Packet",
-        13: "User ID Packet",
-        14: "Public-Subkey Packet",
-        17: "User Attribute Packet",
-        18: "Sym. Encrypted and Integrity Protected Data Packet",
-        19: "Modification Detection Code Packet",
-        60: "Private or Experimental Values",
-        61: "Private or Experimental Values",
-        62: "Private or Experimental Values",
-        63: "Private or Experimental Values",
-    }.get(ptag))
-    assert ptag == 1, "Unsupported packet tag type"
+    def parse_packet_header(self, octets):
+        if (octets[0] & (1<<7)) != (1<<7):
+            raise ValueError("Invalid packet; no leading set bit")
 
-    assert raw[2] == 3, "Unsupported one-octet version number"
+        if (octets[0] & (1<<6)) == (1<<6):
+            raise NotImplementedError("New packet format unsupported")
 
-    version_no = raw[2]
-    print("Version number: %d" % version_no)
+        tag = (octets[0] & 0x3c) >> 2
 
-    keyid = raw[3:3+8]
-    print("KeyID: %s" % "".join(map(lambda s: "%02x" %s, keyid)))
+        if tag == 0:
+            raise ValueError("Packet has illegally set reserved bit")
 
-    pubkey_algo = raw[11]
-    print("Public key algorithm: 0x%02x" % pubkey_algo)
-    print("Public key algorithm: %s" % {
-        1:   "RSA (Encrypt or Sign) [HAC]",
-        2:   "RSA Encrypt-Only [HAC]",
-        3:   "RSA Sign-Only [HAC]",
-        16:  "Elgamal (Encrypt-Only) [ELGAMAL] [HAC]",
-        17:  "DSA (Digital Signature Algorithm) [FIPS186] [HAC]",
-        18:  "Reserved for Elliptic Curve",
-        19:  "Reserved for ECDSA",
-        20:  "Reserved (formerly Elgamal Encrypt or Sign)",
-        21:  "Reserved for Diffie-Hellman (X9.42, as defined for IETF-S/MIME)",
-        100: "Private/Experimental algorithm",
-        101: "Private/Experimental algorithm",
-        102: "Private/Experimental algorithm",
-        103: "Private/Experimental algorithm",
-        104: "Private/Experimental algorithm",
-        105: "Private/Experimental algorithm",
-        106: "Private/Experimental algorithm",
-        107: "Private/Experimental algorithm",
-        108: "Private/Experimental algorithm",
-        109: "Private/Experimental algorithm",
-        110: "Private/Experimental algorithm",
-    }.get(pubkey_algo))
+        length_type = (octets[0] & 0x03)
 
-    encrypted_session_key = raw[12:bodylen-12+1]
-    print("Encrypted session key length: %d" % len(encrypted_session_key))
+        if length_type == 3:
+            raise NotImplementedError("Indeterminate length packets unsupported")
 
-    return filename, version, data
+        header_length = {0: 2, 1: 3, 2: 5}[length_type]
+
+        body_length = {
+            0: octets[1],
+            1: (octets[1]<<8) + octets[2],
+            2: (octets[1]<<24) + (octets[2]<<16) + (octets[3]<<8) + octets[4],
+        }[length_type]
+
+        print("Header length: %d" % header_length)
+        print("Body length: %d" % body_length)
+
+        body = octets[header_length:body_length - header_length]
+        return tag, body
+
+    def parse_packet_contents(self, tag, octets):
+        print("Tag (0x%02x): %s" % (tag, self.tags[tag]))
+
+        if tag == 0:
+            raise ValueError("Packet has illegally set reserved bit")
+
+        parsers = {
+            1: self.parse_pk_encrypted_session_key_packet,
+            2: self.parse_signature_packet
+        }
+
+        try:
+            parser = parsers[tag]
+            return parser(octets)
+        except KeyError:
+            raise NotImplementedError("Unsupported packet: %s" % self.tags[tag])
+
+    def parse_pk_encrypted_session_key_packet(self, octets):
+        version = octets[0]
+        print("PK Version: %d" % version)
+
+        if version != 3:
+            raise NotImplementedError("Unsupported PK Session Key version %d" %
+                    version)
+
+        key_id = octets[1:9]
+        self.contents["key_id"] = key_id
+        print("PK Key ID: %s" % "".join(map(lambda s: "%02x" %s, key_id)))
+
+        pk_algo = octets[10]
+        self.contents["pk_algo"] = pk_algo
+
+        print("Public key algorithm: 0x%02x" % pk_algo)
+        print("PK Algorithm (0x%02x): %s" % (pk_algo, self.pk_algos[pk_algo]))
+
+        if pk_algo == 1:
+            raise NotImplementedError("Unsupported public key algorithm")
+
+        encrypted_session_key = octets[11:]
+        print("PK Encrypted Session Key Length: %d" %
+                len(encrypted_session_key))
+
+        # TODO: Parse algo specifics
+
+        return encrypted_session_key
+
+    def parse_signature_packet(self, tag, octets):
+        pass
 
 
 if __name__ == "__main__":
     for file in sys.argv[1:]:
         with open(file, "rt") as f:
-            bruteforce(*decode(*parse(f)))
+            version, data = parse_ascii_armored_file(f)
+            p = Parser(version, data)
