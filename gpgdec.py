acaeafae81969bd935cc24636546f9e5d8bb6e31
Christian Stigen Larsen <csl@csl.name>
2016-03-15 19:24:25 +0100

    More

--- gpgdec.py
+++ gpgdec.py
@@ -10,12 +10,9 @@ import base64
 import sys
 
 def bruteforce(filename, version, data):
-    print("Filename: %s" % filename)
-    print("Version: %s" % version)
-    print("Bytes: %d" % len(data))
-    print("Raw: %s" % repr(data))
+    pass
 
-def load(file):
+def parse(file):
     data = []
     parsing = False
     version = None
@@ -32,8 +29,20 @@ def load(file):
                 data.append(line.strip())
     return file.name, version, base64.decodestring("".join(data))
 
+def decode(filename, version, data):
+    print("Filename: %s" % filename)
+    print("Version: %s" % version)
+    print("Bytes: %d" % len(data))
+
+    raw = map(ord, data)
+    ptag = int(raw[0])
+    assert (ptag & 0x80) == 0x80, "Incorrect PTag"
+    assert (ptag & 0x40) == 0, "Unsupported packet format"
+
+    return filename, version, data
+
 
 if __name__ == "__main__":
     for file in sys.argv[1:]:
         with open(file, "rt") as f:
-            bruteforce(*load(f))
+            bruteforce(*decode(*parse(f)))
